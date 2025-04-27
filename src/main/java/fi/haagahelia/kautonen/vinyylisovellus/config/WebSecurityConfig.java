2d747ed6fbdf6d3b1ad07d397fb56141b87fb3ea
Olmih02 <bhm503@myy.haaga-helia.fi>
2025-04-27 20:36:35 +0200

    ..

--- src/main/java/fi/haagahelia/kautonen/vinyylisovellus/config/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/kautonen/vinyylisovellus/config/WebSecurityConfig.java
@@ -1,25 +1,24 @@
 package fi.haagahelia.kautonen.vinyylisovellus.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    @Autowired
-    private UserDetailsService userDetailsService;
+  
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
           // 1) Salli staattiset resurssit ja login‐sivun GET ilman autentikaatiota
           .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/css/**", "/images/**", "/login").permitAll()
+            .requestMatchers("/css/**", "/images/**", "/login", "/register").permitAll()
             .anyRequest().authenticated()
           )
           // 2) Form‐login /login –sivulla
